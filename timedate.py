668adfcfb0def4f7158a485e4802c380f618c233
sofiatrev <sofiaetrevino01@outlook.com>
2024-01-24 13:44:31 +0000

    Added timedate.py script

---
+++ timedate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A script that returns the date and time
+"""
+
+import sys
+import os
+import datetime
+
+def main():
+    print("The current date and time is: ", datetime.datetime.now())
+
+if __name__ == "__main__":
+    main()
